da3e1c92762c92364717e2a4ed4a2f7293d267a2
Hasnat-Sajid <2210859008@fh-burgenland.at>
2023-10-19 18:36:56 +0200

    feat(CalculatorService): implement divide method and add Javadoc

--- src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
+++ src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
@@ -2,12 +2,23 @@ package io.muehlbachler.bswe3b;
 
 import org.springframework.stereotype.Service;
 
-// FIXME: add javadoc
+/**
+ * Service class for calculator operations
+ */
 @Service
 public class CalculatorService {
-    // FIXME: add javadoc
+
+    /**
+     * Divides the first argument by the second argument
+     *
+     * @param a the dividend
+     * @param b the divisor
+     * @return the result of the division, or 0 if b is less than 0
+     */
     public int divide(final int a, final int b) {
-        // FIXME: implement
-        return -1;
+        if (b < 0) {
+            return 0;
+        }
+        return a / b;
     }
 }
